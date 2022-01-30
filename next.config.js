c131ab9759f5f058b4100d04d36d95ec8d89bf08
Jorge <perez.jorge718@gmail.com>
2022-01-29 23:46:56 -0400

    image optimization

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 const withPlugins = require('next-compose-plugins');
-// const optimizedImages = require('next-optimized-images');
+const optimizedImages = require('next-optimized-images');
 
 module.exports = withPlugins([
-  // [optimizedImages, {
-  //   mozjpeg: {
-  //     quality: 80,
-  //   },
-  //   pngquant: {
-  //     speed: 3,
-  //     strip: true,
-  //     verbose: true,
-  //   },
-  //   // imagesPublicPath: '/FFXIV-Rotation-Overlay/.next/static/images/',
-  // }],
+  [optimizedImages, {
+    mozjpeg: {
+      quality: 80,
+    },
+    pngquant: {
+      speed: 3,
+      strip: true,
+      verbose: true,
+    },
+    imagesPublicPath: '/FFXIV-Rotation-Overlay/.next/static/images/',
+  }],
   
   {
     basePath: '/FFXIV-Rotation-Overlay',
