9b49876f60b39fd51444f7d8ff359b824d727081
Jorge <perez.jorge718@gmail.com>
2022-01-30 00:50:31 -0400

    change config

--- next.config.js
+++ next.config.js
@@ -2,21 +2,23 @@ const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 
 module.exports = withPlugins([
-  [optimizedImages, {
-    mozjpeg: {
-      quality: 80,
-    },
-    pngquant: {
-      speed: 3,
-      strip: true,
-      verbose: true,
-    },
-    imagesPublicPath: '/FFXIV-Rotation-Overlay/.next/static/images/',
-  }],
+  // [optimizedImages, {
+  //   mozjpeg: {
+  //     quality: 80,
+  //   },
+  //   pngquant: {
+  //     speed: 3,
+  //     strip: true,
+  //     verbose: true,
+  //   },
+  //   imagesPublicPath: '/FFXIV-Rotation-Overlay/.next/static/images/',
+  // }],
   
   {
     images: {
       domains: ['xivapi.com'],
+      loader: 'akamai',
+      path: '',
     },
     basePath: '/FFXIV-Rotation-Overlay',
     assetPrefix: '/FFXIV-Rotation-Overlay/',
