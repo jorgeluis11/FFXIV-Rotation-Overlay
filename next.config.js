b8b1c80e45e1bfb39f2cf7be4396d46f694b0308
Jorge <perez.jorge718@gmail.com>
2022-01-29 23:27:24 -0400

    fix next.config

--- next.config.js
+++ next.config.js
@@ -13,9 +13,11 @@ module.exports = withPlugins([
   //   },
   //   // imagesPublicPath: '/FFXIV-Rotation-Overlay/.next/static/images/',
   // }],
+  
   {
     basePath: '/FFXIV-Rotation-Overlay',
     assetPrefix: '/FFXIV-Rotation-Overlay/',
+    "presets": ["next/babel"]
     // env,
   },
 ]);
