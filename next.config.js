d35bbbce35d790fcd367f4617a1de5a55401f4de
Jorge <perez.jorge718@gmail.com>
2022-01-29 21:50:46 -0400

    config changes

--- next.config.js
+++ next.config.js
@@ -1,3 +1,27 @@
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
 module.exports = {
   reactStrictMode: true,
+  assetPrefix: '/FFXIV-Rotation-Overlay/',
+  basePath: '/FFXIV-Rotation-Overlay',
 }
+
+module.exports = withPlugins([
+  [optimizedImages, {
+    mozjpeg: {
+      quality: 80,
+    },
+    pngquant: {
+      speed: 3,
+      strip: true,
+      verbose: true,
+    },
+    imagesPublicPath: '/wallisconsultancy/_next/static/images/',
+  }],
+  {
+    basePath: '/wallisconsultancy',
+    assetPrefix: '/wallisconsultancy/',
+    env,
+  },
+]);
